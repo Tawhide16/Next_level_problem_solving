83dc90624258806b3f725eadab06349df6192a94
Tawhide16 <tawhideh.b10@gmail.com>
2025-12-04 14:39:33 +0600

    add the all code in one file

--- problem7.ts
+++
@@ -1,7 +0,0 @@
-function getUniqueValues(arr1: (number | string)[], arr2: (number | string)[]) {
-  return Array.from(new Set([...arr1, ...arr2]));
-}
-
-const array1 = [1, 2, 3, 4, 5];
-const array2 = [3, 4, 5, 6, 7];
-
